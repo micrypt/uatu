68ebcc592a3256ab0615b5bf39b4a3c3e6b1e85f
Seyi Ogunyemi <micrypt@gmail.com>
2014-03-29 22:16:08 +0000

    uatu: initialise.

---
+++ app/project.clj
@@ -0,0 +1,6 @@
+(defproject app "0.1.0-SNAPSHOT"
+  :description "Uatu is a distributed application monitoring framework"
+  :url "http://micrypt.com/uatu"
+  :license {:name "Espian Public Domain License"
+            :url "http://www.espians.com/license"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ app/src/app/core.clj
@@ -0,0 +1,6 @@
+(ns app.core)
+
+(defn init
+  "Initialise the watcher"
+  [x]
+  (println x "Hello, World!"))
